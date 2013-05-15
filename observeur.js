00d744d733d3fb129e83c207c2ceebed649bebf1
shinoda13 <loic.faizant@gmail.com>
2013-05-15 11:20:11 +0200

    Version BETA du gradient conjugué

--- observeur.js
+++ observeur.js
@@ -12,9 +12,12 @@ Quintus.Observeur = function(Q) {
       this.V = 10;
       this.angleRotation = (p.angle ? p.angle : 0);
       this.mobile = p.mobile;
-      this.teta = new Array();
-	  this.ci = new Array();
       this.vxSuivre = this.vySuivre = 5;
+	  
+	  this.teta = new Array();
+	  this.ci = new Array();
+	  this.nbMesures = 0;
+	  this.trajectoireMobile;
     },
     // fonction appelé à cheque boucle du jeu
     step: function(dt) {
@@ -31,90 +34,119 @@ Quintus.Observeur = function(Q) {
 	  
       this.teta.push({x:this.X, y:this.Y,
         angle:(Math.atan2((this.mobile.Y - this.Y), (this.mobile.X - this.X)))});
+		
+	  this.nbMesures++;
+	  
+	  if(this.nbMesures == 16) {
+		this.trajectoireMobile = this.gradientConjugue();
+		//console.log(this.trajectoireMobile);
+	  }
     },
+	
     gradientConjugue: function() {
-      // Construire la matrice ci(t)
+	  // Construire la matrice ci(t)
       var ci = new Array();
       for(var i = 0; i < this.teta.length; i++) {
-        ci.push($M(
+        /*ci.push($M(
           [-(Math.cos(this.teta[i].angle))],
           [Math.sin(this.teta[i].angle)],
           [i * Math.sin(this.teta[i].angle)],
           [i * Math.cos(this.teta[i].angle)])
+        );*/
+		ci.push($M([
+		  [Math.sin(this.teta[i].angle)],
+          [-(Math.cos(this.teta[i].angle))],
+          [i * Math.sin(this.teta[i].angle)],
+          [-i * Math.cos(this.teta[i].angle)]])
         );
+		
+		console.log(ci[i]);
       }
-      
-      // Construire la matrice y(t)
+	  
+	  // Construire la matrice y(t)
       var y = new Array();
       for(var i = 0; i < this.teta.length; i++) {
         y.push((-(this.teta[i].y) * Math.sin(this.teta[i].angle)) + (this.teta[i].x * Math.cos(this.teta[i].angle)));
       }
-      
-      // Construire la matrice Gamma
-      var gamma = Matrix.Zero(4, 4);
+	  
+	  // Construire la matrice Gamma
+      var gamma = $M([
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]
+	  ]);
       for(var i = 0; i < ci.length; i++) {
-        gamma.e(1, 1) += ci[i].e(1, 1) * ci[i].e(1, 1);
-        gamma.e(1, 2) += ci[i].e(1, 1) * ci[i].e(2, 1);
-        gamma.e(1, 3) += ci[i].e(1, 1) * ci[i].e(3, 1);
-        gamma.e(1, 4) += ci[i].e(1, 1) * ci[i].e(4, 1);
-        
-        gamma.e(2, 1) += ci[i].e(2, 1) * ci[i].e(1, 1);
-        gamma.e(2, 2) += ci[i].e(2, 1) * ci[i].e(2, 1);
-        gamma.e(2, 3) += ci[i].e(2, 1) * ci[i].e(3, 1);
-        gamma.e(2, 4) += ci[i].e(2, 1) * ci[i].e(4, 1);
-        
-        gamma.e(3, 1) += ci[i].e(3, 1) * ci[i].e(1, 1);
-        gamma.e(3, 2) += ci[i].e(3, 1) * ci[i].e(2, 1);
-        gamma.e(3, 3) += ci[i].e(3, 1) * ci[i].e(3, 1);
-        gamma.e(3, 4) += ci[i].e(3, 1) * ci[i].e(4, 1);
-        
-        gamma.e(4, 1) += ci[i].e(4, 1) * ci[i].e(1, 1);
-        gamma.e(4, 2) += ci[i].e(4, 1) * ci[i].e(2, 1);
-        gamma.e(4, 3) += ci[i].e(4, 1) * ci[i].e(3, 1);
-        gamma.e(4, 4) += ci[i].e(4, 1) * ci[i].e(4, 1);
-      }
+	    /*var tmp = $M([
+			[ci[i].e(1, 1) * ci[i].e(1, 1), ci[i].e(1, 1) * ci[i].e(2, 1), ci[i].e(1, 1) * ci[i].e(3, 1), ci[i].e(1, 1) * ci[i].e(4, 1)],
+			[ci[i].e(2, 1) * ci[i].e(1, 1), ci[i].e(2, 1) * ci[i].e(2, 1), ci[i].e(2, 1) * ci[i].e(3, 1), ci[i].e(2, 1) * ci[i].e(4, 1)],
+			[ci[i].e(3, 1) * ci[i].e(1, 1), ci[i].e(3, 1) * ci[i].e(2, 1), ci[i].e(3, 1) * ci[i].e(3, 1), ci[i].e(3, 1) * ci[i].e(3, 1)],
+			[ci[i].e(4, 1) * ci[i].e(1, 1), ci[i].e(4, 1) * ci[i].e(2, 1), ci[i].e(4, 1) * ci[i].e(3, 1), ci[i].e(4, 1) * ci[i].e(4, 1)]
+		]);*/
+		var tmp = ci[i].multiply(ci[i].transpose());
+		
+		//console.log(ci[i]);
+		
+		gamma = gamma.add(tmp);
+      }     
       
       // Construire la matrice b
-      var b = Matrix.Zero(4, 1);
+      var b = $M([
+		[0],
+		[0],
+		[0],
+		[0]
+	  ]);
       for(var i = 0; i < ci.length; i++) {
-        b.e(1, 1) += ci[i].e(1, 1) * y[i];
-        b.e(2, 1) += ci[i].e(2, 1) * y[i];
-        b.e(3, 1) += ci[i].e(3, 1) * y[i];
-        b.e(4, 1) += ci[i].e(4, 1) * y[i];
+	    var tmp = $M([
+			[ci[i].e(1, 1) * y[i]],
+			[ci[i].e(2, 1) * y[i]],
+			[ci[i].e(3, 1) * y[i]],
+			[ci[i].e(4, 1) * y[i]]
+		]);
+		
+		b = b.add(tmp);
       }
       
       // Initialiser les inconnues
-      var x = Matrix.Zero(4, 1);
+      var x = $M([
+		[0],
+		[0],
+		[0],
+		[0]
+	  ]);
       // Initialiser la matrice de gradients
-      var g = gamma.multiply(x);
-      g = g.subtract(b);
+      var g = (gamma.multiply(x)).subtract(b);
       // Initialiser la matrice de gradients conjugués
       var h = g;
       
       // Déterminer les inconnues
       for(var i = 0; i < 4; i++) {
-        x = x.subtract(
-            h.multiply(
-              (h.transpose().multiply(g)).multiply(
-                (h.transpose().multiply(gamma).multiply(h)).inverse()
-              )
-            )
-          );
-        
-        var old_g = g;
-        g = gamma.multiply(x);
-        g = g.subtract(b);
-        
-        h = g.add(
-            h.multiply(
-              (g.transpose().multiply(g)).multiply(
-                (old_g.transpose().multiply(old_g)).inverse()
-              )
-            )
-          );
+		x = x.subtract(
+			h.multiply(
+				((h.transpose()).multiply(g)).multiply(
+					(((h.transpose()).multiply(gamma)).multiply(h)).inverse()
+				)
+			)
+		);
+		g = g.subtract(
+			(gamma.multiply(h)).multiply(
+				((h.transpose()).multiply(g)).multiply(
+					(((h.transpose()).multiply(gamma)).multiply(h)).inverse()
+				)
+			)
+		);		
+		h = g.subtract(
+			h.multiply(
+				(((h.transpose()).multiply(gamma)).multiply(g)).multiply(
+					(((h.transpose()).multiply(gamma)).multiply(h)).inverse()
+				)
+			)
+		);
       }
       
-      // Renvoyer les incconus déterminées
+	  console.log(x);
+      // Renvoyer les inconnues déterminées
       return x;
     }
   });
